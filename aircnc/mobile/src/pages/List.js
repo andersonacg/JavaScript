47aaa0eed547e3082fcedbd4a98025107582f753
andersonacg <andersonaugusto1987@gmail.com>
2019-10-17 20:56:50 -0300

    Atualização dos dados

---
+++ aircnc/mobile/src/pages/List.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect }  from 'react';
+import socketio from 'socket.io-client'
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage, Text, TouchableOpacity } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List({ navigation }) {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.0.5:3333', {
+        query: { user_id }
+      })
+
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REPROVADA'}`);
+      })
+    })
+  },[]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    })
+  }, [])
+
+  function handleLogout() {
+    navigation.navigate('Login');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+      </ScrollView>
+      <TouchableOpacity onPress={handleLogout} style={[styles.button, styles.cancelButton]}>
+        <Text style={styles.buttonText}>Logout</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: 'center',
+    marginTop: 50
+  },
+});
